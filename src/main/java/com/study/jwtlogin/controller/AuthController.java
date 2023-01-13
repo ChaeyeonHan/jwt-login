e808e5c70eb0d00a820eb1c34f46b2cafcab4ecb
ChaeyeonHan <kate2236e@naver.com>
2023-01-13 14:32:10 +0900

    mod : Response에 맞게 반환값 수정

--- src/main/java/com/study/jwtlogin/controller/AuthController.java
+++ src/main/java/com/study/jwtlogin/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.study.jwtlogin.controller;
 
+import com.study.jwtlogin.config.BaseResponse;
 import com.study.jwtlogin.dto.*;
 import com.study.jwtlogin.service.AuthService;
 import com.study.jwtlogin.service.UserService;
@@ -22,13 +23,13 @@ public class AuthController {
 
 
     @PostMapping("/signup")  // 회원가입 API
-    public ResponseEntity<LoginRes> signup(@Valid @RequestBody JoinRequestDto joinRequestDto) {
+    public BaseResponse<LoginRes> signup(@Valid @RequestBody JoinRequestDto joinRequestDto) {
         LoginRes loginRes = userService.signup(joinRequestDto);
-        return new ResponseEntity<>(loginRes, null, HttpStatus.OK);
+        return new BaseResponse<>(loginRes);
     }
 
     @GetMapping("/form-login")  // 로그인 API
-    public ResponseEntity<TokenRes> formLogin(@RequestBody LoginRequestDto loginDto) {
+    public BaseResponse<TokenRes> formLogin(@RequestBody LoginRequestDto loginDto) {
         // 로그인 요청이 들어오면, loginDto로 받아온 email, password로 사용자 인증을 진행하고
         // UserPasswordAuthenticationToken 객체를 생성하고, password가 일치하는지 확인
         // 검증이 정상적으로 진행되었다면, JWT 토큰을 생성(accessToken, refreshToken 모두 발급)하고 저장한다
@@ -39,12 +40,15 @@ public class AuthController {
 
         System.out.println(loginDto.getEmail() + "의 유저 로그인 완료");
 
-        return new ResponseEntity<>(res,null, HttpStatus.OK);  // 바디, 헤더, 상태코드
+        return new BaseResponse<>(res);
+//        return new ResponseEntity<>(res,null, HttpStatus.OK);  // 바디, 헤더, 상태코드
     }
 
     @PostMapping("/reissue")  // 토큰 재발급 API
-    public void reissue(@RequestBody TokenRequestDto tokenRequestDto) {
-        authService.reissue(tokenRequestDto);
+    public BaseResponse<TokenRes> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
+        TokenRes reissueToken = authService.reissue(tokenRequestDto);
+
+        return new BaseResponse<>(reissueToken);
     }
 
 }
